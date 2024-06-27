c133458769c46e7fdcc77edb2360b86032d14158
Basliel Tegegn <basgolilo5@gmail.com>
2024-06-27 08:15:35 -0700

    ad

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,29 +1,20 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  /**
-   * Constructs a new SkyHighBuilding object.
-   * @param {Number} sqft - The square footage of the building.
-   * @param {Number} floors - The number of floors in the building.
-   */
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
-  /**
-   * Getter for the number of floors.
-   * @returns {Number} The number of floors.
-   */
   get floors() {
     return this._floors;
   }
 
-  /**
-   * Override of the evacuationWarningMessage method.
-   * @returns {String} Evacuation message based on the number of floors.
-   */
+  set floors(value) {
+    this._floors = value;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
