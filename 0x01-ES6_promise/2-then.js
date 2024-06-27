b09a01e2e7324daed8cac484b859f406f86726b3
Basliel Tegegn <basgolilo5@gmail.com>
2024-06-27 06:32:37 -0700

    ad

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,10 @@
-async function handleResponseFromAPI(promise) {
-  try {
-    await promise;
-    return {
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
       status: 200,
       body: 'success',
-    };
-  } catch (error) {
-    throw new Error();
-  } finally {
-    console.log('Got a response from the API');
-  }
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-
 export default handleResponseFromAPI;
