95b218d01e4f984b4ab1d8ba044bfb0244ff2a7e
Basliel Tegegn <basgolilo5@gmail.com>
2024-06-27 07:34:53 -0700

    ad

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+    
+    // Verify types during object creation
+    if (typeof this._name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof this._length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(this._students) || !this._students.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Getter and setter for length
+  get length() {
+    return this._length;
+  }
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Getter and setter for students
+  get students() {
+    return this._students;
+  }
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
