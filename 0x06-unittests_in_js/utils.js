26275b389bae59782277dc1a9dcd560a8ca7e726
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-22 07:08:03 -0700

    fix the

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -2,23 +2,23 @@ class Utils {
     static calculateNumber(type, a, b) {
 	const an = Math.round(a);
 	const bn = Math.round(b);
-	let c = 0;
+	let d = 0;
 	switch (type) {
 	case 'SUM':
-            c = an + bn;
+            d = an + bn;
             break;
 	case 'SUBTRACT':
-            c = an - bn;
+            d = an - bn;
             break;
 	case 'DIVIDE':
 	    if (bn === 0) {
-		c = "Error";
+		d = "Error";
 	    } else {
-		c = an / bn;
+		d = an / bn;
 	    }
 	    break;
 	}
-	return c;
+	return d;
     }
 }
 
