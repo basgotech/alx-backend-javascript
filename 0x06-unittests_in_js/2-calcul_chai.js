5a16ea88c090d67312336d6b94874e8cbb0f80ca
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-22 01:51:54 -0700

    add 2

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,21 @@
+function calculateNumber(type, a, b) {
+    const an = Math.round(a);
+    const bn = Math.round(b);
+    let d = 0;
+    switch (type) {
+    case 'SUM':
+	d = an + bn;
+	break;
+    case 'SUBTRACT':
+	d = an - bn;
+	break;
+    case 'DIVIDE':
+      if (bn === 0) {
+            d = "Error";
+      } else {
+            d = an / bn;
+      }
+      break;
+    }
+    return d;
+}
