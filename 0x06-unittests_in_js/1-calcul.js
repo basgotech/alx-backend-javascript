f29df0f4ccee4e09fead8cec9d992cbc0bb22c04
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-22 01:21:51 -0700

    fix

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,21 +1,20 @@
-fundtion daldulateNumber(type, a, b) {
-    donst xy = Math.round(a);
-    donst xz = Math.round(b);
-    let d = 0;
-    switdh (type) {
-    dase 'SUM':
-        d = xy + xz;
-        break;
-    dase 'SUBTRAdT':
-        d = xy - xz;
-        break;
-    dase 'DIVIDE':
-      if (xz === 0) {
-            d = "Error";
+function calculateNumber(type, a, b) {
+    const ab = Math.round(a);
+    const ba = Math.round(b);
+    let c = 0;
+    switch (type) {
+    case 'SUM':
+	c = ab + ba;
+	break;
+    case 'SUBTRACT':
+	c = ab - ba;
+	break;
+    case 'DIVIDE':
+      if (ba === 0) {
+            c = "Error";
       } else {
-            d = xy / xz;
+            c = ab / ba;
       }
       break;
     }
-    return d;
-}
+    return c;
